cd2a17f599fef04afb66e3f0605a0c41d6d865ec
BurningBlaze <pmdblaze@tutanota.com>
2020-03-22 03:21:26 -0400

    Set date for weekly event

--- Commands.cs
+++ Commands.cs
@@ -376,7 +376,7 @@ namespace Script
                         {
                             if (Ranks.IsAllowed(client, Enums.Rank.Scripter))
                             {
-                                var startTime = new DateTime(DateTime.UtcNow.Year, 3, 8, 17, 0, 0, DateTimeKind.Utc);
+                                var startTime = new DateTime(DateTime.UtcNow.Year, 3, 22, 17, 0, 0, DateTimeKind.Utc);
 
                                 if (Main.SetEvent(client, joinedArgs, false))
                                 {
