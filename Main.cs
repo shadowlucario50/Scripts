c6da928796358b5477509b8c77b2733ed55fd148
BurningBlaze <pmdblaze@tutanota.com>
2020-03-29 03:39:34 -0400

    Increase fragment spawn rate

--- Main.cs
+++ Main.cs
@@ -9393,7 +9393,7 @@ namespace Script
                         var legendary = LegendaryManager.Instance.Resources.Where(x => x.Id == dungeon.LegendaryId - 1).FirstOrDefault();
                         if (legendary != null && legendary.FragmentItemId > 0)
                         {
-                            if (Server.Math.Rand(0, 1024) == 0)
+                            if (Server.Math.Rand(0, 100) <= 10)
                             {
                                 RandomItemSpawn(map, legendary.FragmentItemId, 1, false, false, string.Empty);
                                 Messenger.PlayerMsg(client, "You feel a strange force on this floor...", Text.BrightGreen);
