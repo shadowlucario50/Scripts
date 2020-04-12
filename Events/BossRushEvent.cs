7ac1f930e4928c98e6bd3e174ce8694b5ce59399
BurningBlaze <pmdblaze@tutanota.com>
2020-04-12 06:03:43 -0400

    Boss buffs

--- Events/BossRushEvent.cs
+++ Events/BossRushEvent.cs
@@ -181,11 +181,11 @@ namespace Script.Events
 
         public override void OnNpcSpawn(IMap map, MapNpcPreset npc, MapNpc spawnedNpc, PacketHitList hitlist)
         {
-            spawnedNpc.MaxHPBonus = 350;
-            spawnedNpc.SpeedBuff = 5;
-            spawnedNpc.SpAtkBuff = 3;
-            spawnedNpc.DefenseBuff = 2;
-            spawnedNpc.SpDefBuff = 2;
+            spawnedNpc.MaxHPBonus = 100;
+            spawnedNpc.SpeedBuff = 1;
+            spawnedNpc.SpAtkBuff = 1;
+            spawnedNpc.DefenseBuff = 1;
+            spawnedNpc.SpDefBuff = 1;
             spawnedNpc.HP = spawnedNpc.MaxHP;
 
             Main.RefreshCharacterTraits(spawnedNpc, map, hitlist);
